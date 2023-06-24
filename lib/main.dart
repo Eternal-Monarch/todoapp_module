81ff8665e1921f5756aebab62f96de1d9c99fe16
subha <subhasishduttshuvo2018@gmail.com>
2023-06-25 00:02:10 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TaskManagementApp());
+}
+
+class TaskManagementApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Management',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: TaskListScreen(),
+    );
+  }
+}
+
+class TaskListScreen extends StatefulWidget {
+  @override
+  _TaskListScreenState createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  List<Task> tasks = [];
+
+  void addTask(Task newTask) {
+    setState(() {
+      tasks.add(newTask);
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+    });
+  }
+
+  void openTaskDetails(int index) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  tasks[index].title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  tasks[index].description,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Days Required: ${tasks[index].daysRequired}',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  deleteTask(index);
+                  Navigator.pop(context);
+                },
+                child: Text('Delete Task'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void openAddTaskDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String title = '';
+        String description = '';
+        int daysRequired = 0;
+
+        return AlertDialog(
+          title: Text('Add New Task'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                onChanged: (value) {
+                  title = value;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 8.0),
+              TextField(
+                onChanged: (value) {
+                  description = value;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 8.0),
+              TextField(
+                onChanged: (value) {
+                  daysRequired = int.tryParse(value) ?? 0;
+                },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Days Required',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                addTask(Task(title, description, daysRequired));
+                Navigator.pop(context);
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task Management'),
+        centerTitle: true, // Align the title in the middle
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onLongPress: () {
+              openTaskDetails(index);
+            },
+            child: Container(
+              margin: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.deepOrange,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: ListTile(
+                title: Text(tasks[index].title),
+                subtitle: Text(tasks[index].description),
+                trailing: Text('${tasks[index].daysRequired} days'),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: openAddTaskDialog,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Task {
+  final String title;
+  final String description;
+  final int daysRequired;
+
+  Task(this.title, this.description, this.daysRequired);
+}
